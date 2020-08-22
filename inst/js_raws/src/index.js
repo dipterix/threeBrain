335a924c13c53942b586f75ab47d63a620182275
dipterix <zw23@rice.edu>
2020-08-22 18:38:46 -0500

    Removed jQuery, bootstrap, webm-writer from js lib

--- inst/js_raws/src/index.js
+++ inst/js_raws/src/index.js
@@ -274,13 +274,18 @@ class BrainCanvas{
     this.canvas.clear_all();
 
     to_array( this.settings.color_maps ).forEach((v) => {
+      // calculate cmap, add time range so that the last value is always displayed
+      let tr = v.time_range;
+      if( tr.length == 2 ){
+        tr[1] += 1.0;
+      }
       this.canvas.add_colormap(
         v.name,
         v.alias,
         v.value_type,
         v.value_names,
         v.value_range,
-        v.time_range,
+        tr,
         v.color_keys,
         v.color_vals,
         v.color_levels,
