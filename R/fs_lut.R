f12ce4a9a70bf2a18cfe3e63c89edb5830005043
dipterix <zw23@rice.edu>
2021-08-05 13:20:59 -0400

    Fixed documentation

--- R/fs_lut.R
+++ R/fs_lut.R
@@ -1,5 +1,18 @@
 
-
+#' @title Query the 'FreeSurfer' labels
+#'
+#' @details The 'FreeSurfer' atlases use
+#' \url{https://surfer.nmr.mgh.harvard.edu/fswiki/FsTutorial/AnatomicalROI/FreeSurferColorLUT}
+#' look-up table to query indexes. The 'threeBrain' electrode localization
+#' also uses this table to export electrode 'FSLabel'. If volume type is
+#' set to \code{'aparc_aseg'}, then please also use this table to filter.
+#'
+#' @examples
+#'
+#' freesurfer_lut$from_key(0:10)
+#'
+#' freesurfer_lut$get_key("ctx-lh-supramarginal")
+#'
 #' @export
 freesurfer_lut <- local({
   cmap <- NULL
