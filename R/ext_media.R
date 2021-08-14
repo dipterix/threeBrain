d76b1f082b41aa0ca4916077bbb59ac51616c521
dipterix <zw23@rice.edu>
2021-08-14 02:25:32 -0400

    corrected docs

--- R/ext_media.R
+++ R/ext_media.R
@@ -1,3 +1,26 @@
+#' Add video content to the viewer
+#' @param path local file path or 'URL'
+#' @param duration duration of the video
+#' @param time_start start time relative to the stimuli onset
+#' @param asp_ratio aspect ratio; default is \code{16/9}
+#' @param local used only when \code{path} is a 'URL': whether to download
+#' the video before generating the viewer; see 'Details'
+#' @details The video path can be either local file path or a 'URL' from
+#' websites. When path is from the internet, there are two options: download
+#' the video before generating the viewer, or directly use the 'URL'.
+#'
+#' If download happens before a viewer is generated (\code{local=TRUE}), then
+#' the video content is local. The viewer will be self-contained. However,
+#' the distribution will contain the video, and the archive size might be large.
+#'
+#' If raw 'URL' is used (\code{local=FALSE}), then viewer is not self-contained
+#' as the video link might break anytime. The 'screenshot' and 'record' function
+#' might be limited if the 'URL' has different domain than yours. However,
+#' the distribution will not contain the video, hence smaller. This works in the
+#' scenarios when it is preferred not to share video files or they are
+#' licensed, or simply distribution is limited. Besides, this method is slightly
+#' faster than the local alternatives.
+#'
 #' @export
 video_content <- function(path, duration = Inf, time_start = 0, asp_ratio = 16 / 9, local = TRUE){
 
@@ -30,6 +53,7 @@ video_content <- function(path, duration = Inf, time_start = 0, asp_ratio = 16 /
   if(is_url){
     url <- path
   } else {
+    path <- normalizePath(path)
     url <- filename(path)
   }
 
