258a7cda546e7740cf38df008786af64b8914c9e
dipterix <dipterix.wang@gmail.com>
2023-07-14 16:09:47 -0400

    added get_ijk2ras to get Norig (sform) and Torig (tkr)

--- R/utils_generate_subcortical_surf.R
+++ R/utils_generate_subcortical_surf.R
@@ -71,12 +71,99 @@ read_volume <- function(file, format = c("auto", "mgh", "nii"), header_only = FA
     Torig <- cbind(Torig, -Torig %*% volume$get_shape() / 2)
     Torig[4, 4] <- 1
   }
-  list(
-    header = header,
-    data = data,
-    Norig = Norig,
-    Torig = Torig
+  structure(
+    list(
+      header = header,
+      data = data,
+      Norig = Norig,
+      Torig = Torig
+    ),
+    class = c("threeBrain.volume", "list")
   )
+}
+
+
+#' @title Get 'voxel' to world matrix
+#' @param x path to imaging files
+#' @param type world space type; choices are \code{'scanner'} (same as
+#' \code{'sform'} or \code{'qform'} in) or \code{'NIfTI'} file headers;
+#' or \code{'tkr'} (used to shared surface nodes)
+#' @returns A four by four matrix
+#' @export
+get_ijk2ras <- function(x, type = c("scanner", "tkr")) {
+  type <- match.arg(type)
+
+  if( inherits(x, "ants.core.ants_image.ANTsImage")) {
+    tmpfile <- tempfile(fileext = ".nii.gz")
+    on.exit({
+      unlink(tmpfile)
+    })
+    x$to_file(normalizePath(tmpfile, mustWork = FALSE))
+    x <- read_volume(tmpfile, header_only = TRUE)
+  }
+
+  if(is.character(x)) {
+    x <- read_volume(x, header_only = TRUE)
+  }
+  if(inherits(x, "threeBrain.volume")) {
+    if(type == "scanner") {
+      return(x$Norig)
+    } else {
+      return(x$Torig)
+    }
+  }
+  if( inherits(x, "mghheader") ) {
+    # Norig: IJK to scanner-RAS
+    Norig <- x$vox2ras_matrix
+    if(type == "scanner") {
+      return(Norig)
+    } else {
+      # Torig: IJK to tkr-RAS
+      Torig <- Norig[1:4, 1:3]
+      Torig <- cbind(Torig, -Torig %*% x$internal$Pcrs_c)
+      Torig[4, 4] <- 1
+      return(Torig)
+    }
+  }
+  if( inherits(x, "oro.nifti") ) {
+    sform_code <- c(x@sform_code, 0)[[1]]
+    qform_code <- c(x@qform_code, 0)[[1]]
+
+    # https://github.com/dipterix/threeBrain/issues/15
+    if(sform_code == 0 && qform_code == 0) {
+      Norig <- diag(c(x@pixdim[seq(2,4)], 1))
+    } else {
+      use_sform <- TRUE
+      prefered_code <- c(1, 4, 2, 5, 3, 0)
+
+      if(which(prefered_code == sform_code) >
+         which(prefered_code == qform_code)) {
+        use_sform <- FALSE
+      }
+      if( use_sform ) {
+        Norig <- rbind(
+          x@srow_x,
+          x@srow_y,
+          x@srow_z,
+          c(0,0,0,1)
+        )
+      } else {
+        Norig <- oro.nifti::qform(x)
+      }
+    }
+
+    if(type == "scanner") {
+      return(Norig)
+    } else {
+      # Torig: IJK to tkr-RAS
+      Torig <- Norig[1:4, 1:3]
+      Torig <- cbind(Torig, -Torig %*% oro.nifti::dim_(x)[c(2,3,4)] / 2)
+      Torig[4, 4] <- 1
+      return(Torig)
+    }
+  }
+
+  stop("get_ijk2ras: Unsupported format")
 }
 
 as_subcortical_label <- function(x, remove_hemisphere = FALSE) {
@@ -87,6 +174,8 @@ as_subcortical_label <- function(x, remove_hemisphere = FALSE) {
       return(k)
     }
   }, "")
+
+  x <- tolower(x)
 
   if( remove_hemisphere ) {
     x <- gsub("^ctx[_-][lr]h", "ctx", x)
@@ -100,8 +189,22 @@ as_subcortical_label <- function(x, remove_hemisphere = FALSE) {
   x
 }
 
-generate_subcortical_surface <- function(path, atlas, index, label = NULL, IJK2RAS = NULL,
-                                         remesh = TRUE, smooth = TRUE, ...) {
+#' @title Approximate 'sub-cortical' surfaces from 'parcellation'
+#' @param atlas path to imaging 'parcellation', can be \code{'nii'} or \code{'mgz'} formats
+#' @param save_prefix parent folder to save the resulting surface
+#' @param index 'parcellation' index, see 'FreeSurfer' look-up table
+#' @param label character label or name of the 'sub-cortical' structure, usually automatically derived from \code{index}
+#' @param IJK2RAS an 'Affine' matrix from 'voxel' index to \code{'tkrRAS'}, usually automatically derived from \code{atlas}
+#' @param grow amount to grow (dilate) before generating mesh
+#' @param remesh,smooth,smooth_delta,... passed to \code{\link[ravetools]{mesh_from_volume}}
+#' @returns A surface mesh, containing 'atlas' index, label, surface nodes and face indices.
+#' @export
+generate_subcortical_surface <- function(atlas, index, save_prefix = NULL, label = NULL, IJK2RAS = NULL, grow = 1,
+                                         remesh = TRUE, smooth = TRUE, smooth_delta = 3, ...) {
+
+  if(is.character(atlas)) {
+    atlas <- read_volume(atlas)
+  }
 
   # atlas_file <- "aparc+aseg.mgz"
   # atlas <- freesurferformats::read.fs.mgh(file.path(path, "mri", "aparc+aseg.mgz"), with_header = TRUE)
@@ -120,6 +223,14 @@ generate_subcortical_surface <- function(path, atlas, index, label = NULL, IJK2R
   label <- tolower(label)
 
   mask <- atlas$data == index
+
+
+  grow <- as.integer(grow)
+  if( grow >= 1) {
+    ravetools <- asNamespace("ravetools")
+    mask <- ravetools$grow_volume(mask, grow)
+  }
+
   mesh <- ravetools::mesh_from_volume(
     volume = mask,
     output_format = "freesurfer",
@@ -127,20 +238,24 @@ generate_subcortical_surface <- function(path, atlas, index, label = NULL, IJK2R
     threshold = 0.5,
     remesh = remesh,
     smooth = smooth,
+    smooth_delta = smooth_delta,
     ...
   )
 
-  save_path <- file.path(path, "surf", "subcortical")
-  if(!dir.exists(save_path)) {
-    dir.create(save_path, showWarnings = FALSE, recursive = TRUE)
+  # save_path <- file.path(path, "surf", "subcortical")
+  # if(!dir.exists(save_path)) {
+  #   dir.create(save_path, showWarnings = FALSE, recursive = TRUE)
+  # }
+  mesh$atlas_label <- as_subcortical_label(label)
+  mesh$atlas_index <- as_subcortical_label(index)
+  if(length(save_prefix) == 1) {
+    freesurferformats::write.fs.surface(
+      filepath = file.path(save_prefix, sprintf("%s-%s", label, index)),
+      vertex_coords = mesh$vertices,
+      faces = mesh$faces,
+      format = "bin"
+    )
   }
-  label <- as_subcortical_label(label)
-  freesurferformats::write.fs.surface(
-    filepath = file.path(save_path, sprintf("%s-%s", label, index)),
-    vertex_coords = mesh$vertices,
-    faces = mesh$faces,
-    format = "bin"
-  )
 
   invisible(mesh)
 }
