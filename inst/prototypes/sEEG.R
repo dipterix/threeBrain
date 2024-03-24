0ef6c1c35f97cdad85e1c5db04c6316f8527d4ba
dipterix <dipterix.wang@gmail.com>
2024-03-24 00:06:54 -0400

    fixed UV issues in prototype

--- inst/prototypes/sEEG.R
+++ inst/prototypes/sEEG.R
@@ -31,12 +31,12 @@ seeg_prototype <- function(
   radius0 <- radius
   paths <- c(
     radius * (1 - cos(pi / 8 * (seq_len(5) - 1))),
-    center_position, max_p,
+    center_position, max_p, max_p + 0.01,
     max_e
   )
   radius <- c(
     radius * sin(pi / 8 * (seq_len(5) - 1)),
-    rep(radius, npos + 2)
+    rep(radius, npos + 3)
   )
   pr <- cbind(paths, radius)
   n_layers <- length(paths)
@@ -44,29 +44,35 @@ seeg_prototype <- function(
   x <- cos((seq_len(width_segments) - 1) * 2 * pi / width_segments)
   y <- sin((seq_len(width_segments) - 1) * 2 * pi / width_segments)
 
-  uw <- 0.5 / width_segments
   uvu <- 1 / (width_segments - 1) * (seq_len(width_segments) - 1)
+  uvu[[1]] <- 0.0001
 
   positions_n_uv <- apply(pr, 1L, function(zr) {
     z <- zr[[1]]
     r <- zr[[2]]
+
+    if( z == 0 ) {
+      x <- 0.0001
+    }
     rbind(x * r, y * r, z, uvu, z / max_p)
   })
 
   nverts <- length(paths) * width_segments
   dim(positions_n_uv) <- c(5, nverts)
-  nverts <- nverts + 1
-  positions_n_uv <- cbind(c(0,0,0,0,0), positions_n_uv)
+  positions_n_uv <- cbind(c(0,0,0,0,0), positions_n_uv, c(0, 0, max_e, 2, 2))
+  nverts <- nverts + 2
+
+  positions_n_uv[c(4,5), nverts + 1 - seq_len(width_segments * 2 + 1)] <- 2
 
   position <- positions_n_uv[1:3, ]
   uv <- positions_n_uv[4:5, ]
-  uv[, nverts + 1 - seq_len(width_segments)] <- 2
 
   # construct face index
   side_cover <- sapply(seq_len(width_segments), function(ii) {
     jj <- ifelse(ii == width_segments, 1, ii + 1)
     c(ii, 0, jj)
   })
+  side_cover <- as.vector(side_cover)
 
   height_index_base <- sapply(seq_len(width_segments), function(ii) {
     jj <- ifelse(ii == width_segments, 1, ii + 1)
@@ -75,7 +81,7 @@ seeg_prototype <- function(
   height_index <- sapply(seq_len(n_layers - 1), function(layer) {
     height_index_base + (layer - 1) * width_segments
   })
-  index <- c(as.vector(side_cover), as.vector(height_index))
+  index <- c(side_cover, as.vector(height_index), nverts - side_cover - 1L)
 
   texture_size <- c(4, 256)
   uv_start <- (center_position - widths / 2) / max_p
@@ -84,6 +90,13 @@ seeg_prototype <- function(
   texture_start <- floor(texture_size[[2]] * uv_start) + 1L
   texture_end <- ceiling(texture_size[[2]] * uv_end) + 1L
   channel_map <- rbind(1, texture_start, 4, texture_end - texture_start)
+
+  mesh <- ravetools::vcg_update_normals(
+    list(
+      vb = position,
+      it = index + 1L
+    )
+  )
 
   config <- list(
     type = "sEEG-16",
@@ -96,13 +109,15 @@ seeg_prototype <- function(
     geometry = "CustomGeometry",
 
     # whether using UV mapping to derive outlines rather than interactively determine the outlines
-    fix_outline = FALSE,
+    fix_outline = TRUE,
 
     transform = diag(1, 4L),
 
     position = position,
 
     index = index,
+
+    normal = mesh$normals[1:3, , drop = FALSE],
 
     uv = uv,
 
@@ -137,6 +152,14 @@ proto$as_json(to_file = "inst/prototypes/sEEG-16.json", flattern = TRUE)
 #
 # proto$preview_3d()
 mesh <- proto$as_mesh3d()
+mesh$vb[3,] <- mesh$vb[3,] / 50
 ravetools::rgl_view({
-  ravetools::rgl_call("shade3d", mesh)
+  ravetools::rgl_call("wire3d", mesh, col = "red")
 })
+
+
+
+brain <- raveio::rave_brain("devel/mni152_b")
+brain$electrodes$remote_geometry(prototype_name = "sEEG-16", delete = TRUE)
+brain <- raveio::rave_brain("devel/mni152_b")
+brain$plot(debug=TRUE)
